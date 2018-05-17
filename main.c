c3812d43608a0c0134000ee815f035b44082fc3d
LuWenYi <loveqq0409@gmail.com>
2018-05-17 09:43:35 +0800

    main

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 	int MathGrade,EnglishGrade,AverageValue;
 int main(int argc, char *argv[]) {
 
-	printf("MathGrade=");
-	scanf("%d",&MathGrade);
-	printf("EnglishGrade=");
-	scanf("%d",&EnglishGrade);
-	AverageValue=(MathGrade+EnglishGrade)/2;
-	printf("AverageValue=%d",AverageValue);
-
+	printf("MathGrade=");/*印出提示字元讓使用者輸入數學成績*/
+	scanf("%d",&MathGrade);/*輸入數學成績*/
+	printf("EnglishGrade=");/*印出提示字元讓使用者輸入英文成績*/
+	scanf("%d",&EnglishGrade);/*輸入英文成績*/
+	AverageValue=(MathGrade+EnglishGrade)/2;/*計算平均成績*/
+	printf("AverageValue=%d",AverageValue);/*印出平均成績*/
+	
 }
